f1d264b8152f589f5ae7d4facb2ec6a8a20a48d7
mason <sdy1819@163.com>
2022-06-27 17:43:37 +0800

    chapter01

---
+++ taco-cloud/src/main/java/com/mason/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,17 @@
+package com.mason.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:SpringBoot启动类
+ * @author mason
+ */
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
